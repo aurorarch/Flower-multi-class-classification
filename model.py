45685a9bb593bf8849f05b81f878987c5a1be824
Pulkit Singh <44130480+deadpoolkit@users.noreply.github.com>
2019-09-27 18:25:52 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+import numpy as np
+
+
